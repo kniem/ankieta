a4360e34efae8270c7a5ff5084c51361bab201fd
Piotr <piotrolbrot@gmail.com>
2016-05-28 18:31:54 +0200

    some alchemy??/

--- app.py
+++ app.py
@@ -167,5 +167,10 @@ def save():
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
+
     app.debug = True
     app.run()
